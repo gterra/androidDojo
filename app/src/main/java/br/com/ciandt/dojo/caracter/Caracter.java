023b996720ffddd1de61e97bc2c461998b6945c6
Vitor Nascimento de Araújo <vnaraujo@ciandt.com>
2016-07-05 21:31:15 -0300

    create splash screen

---
+++ app/src/main/java/br/com/ciandt/dojo/caracter/Caracter.java
@@ -0,0 +1,18 @@
+package br.com.ciandt.dojo.caracter;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import br.com.ciandt.dojo.myapplication.R;
+
+/**
+ * Created by vnaraujo on 05/07/2016.
+ */
+public class Caracter extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.caracter);
+    }
+}
--- app/src/main/java/br/com/ciandt/dojo/myapplication/MainActivity.java
+++ app/src/main/java/br/com/ciandt/dojo/myapplication/MainActivity.java
@@ -1,21 +1,41 @@
 package br.com.ciandt.dojo.myapplication;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.res.AssetManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
-
 import com.squareup.picasso.Picasso;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Timer;
+import java.util.TimerTask;
+
+import br.com.ciandt.dojo.caracter.Caracter;
+
+public class MainActivity extends Activity implements Runnable{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
+        ImageView imageView = (ImageView) findViewById(R.id.img_splash);
+        Picasso.with(getApplicationContext()).load(R.mipmap.splash_screen).into(imageView);
+        this.run();
+    }
 
-        ImageView img = (ImageView) findViewById(R.id.img_splah);
-        Picasso.with(this).load(R.mipmap.ic_launcher).into(img);
-
-
+    @Override
+    public void run() {
+        try {
+            new Timer().schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    startActivity(new Intent(getApplicationContext(), Caracter.class));
+                }
+            },2000);
+        }catch (Exception ex){
+            Log.e("Error",ex.toString());
+        }
     }
 }
